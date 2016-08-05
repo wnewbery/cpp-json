d36c6823ccc3c0f04fb6b66b39f13e1ab5135200
William Newbery <wnewbery@hotmail.co.uk>
2016-08-05 23:55:21 +0100

    Optimised parse_iso_time

--- include/json/Time.hpp
+++ include/json/Time.hpp
@@ -1,35 +1,55 @@
 #pragma once
 #include <ctime>
 #include <string>
-#include <sstream>
 #include <stdexcept>
 
 namespace json
 {
+    namespace
+    {
+        int parse_time_num(char c, const std::string &time_str)
+        {
+            if (c >= '0' && c <= '9') return c - '0';
+            else throw std::runtime_error("Invalid time " + time_str);
+        }
+        template<size_t len>
+        int parse_time_num(const char *str, const std::string &time_str)
+        {
+            int sum = 0;
+            for (size_t i = 0; i < len; ++i)
+            {
+                sum *= 10;
+                sum += parse_time_num(str[i], time_str);
+            }
+            return sum;
+        }
+    }
     /**Parse time. Currently supports only UTC 'YYYY-MM-DDTHH:MM:SS' with optional trailing Z.*/
     inline time_t parse_iso_time(const std::string &str)
     {
+        auto strp = str.c_str();
+        auto len = str.size();
+        if (len < sizeof("YYYY-MM-DDTHH:MM:SS") - 1) throw std::runtime_error("Invalid time " + str);
+        if (len > sizeof("YYYY-MM-DDTHH:MM:SSZ") - 1) throw std::runtime_error("Invalid time " + str);
+        //validate static components
+        if (len == sizeof("YYYY-MM-DDTHH:MM:SSZ") - 1 && str[19] != 'Z') throw std::runtime_error("Invalid time " + str);
+        if (strp[4] != '-' || strp[7] != '-' || strp[10] != 'T' || strp[13] != ':' || strp[16] != ':')
+            throw std::runtime_error("Invalid time " + str);
+        //parse numbers
         tm tm;
-        char sep;
 
-        std::stringstream ss(str);
-        ss >> tm.tm_year >> sep >> tm.tm_mon >> sep >> tm.tm_mday;
-        ss >> sep;
-        ss >> tm.tm_hour >> sep >> tm.tm_min >> sep >> tm.tm_sec;
+        tm.tm_year = parse_time_num<4>(strp + 0, str);
+        tm.tm_mon = parse_time_num<2>(strp + 5, str);
+        tm.tm_mday = parse_time_num<2>(strp + 8, str);
 
-        if (ss.fail()) throw std::runtime_error("Invalid time " + str);
-        ss >> sep;
-        if (ss.good())
-        {
-            if (sep != 'Z') throw std::runtime_error("Invalid time " + str);
-        }
-        ss >> sep;
-        if (!ss.eof()) throw std::runtime_error("Invalid time " + str);
-
+        tm.tm_hour = parse_time_num<2>(strp + 11, str);
+        tm.tm_min = parse_time_num<2>(strp + 14, str);
+        tm.tm_sec = parse_time_num<2>(strp + 17, str);
+        //adjust for tm
         tm.tm_year -= 1900;
         tm.tm_mon -= 1;
         tm.tm_isdst = 0;
-
+        //validate
         if (tm.tm_year < 0 || tm.tm_year > 1100 ||
             tm.tm_mon < 0 || tm.tm_mon > 11 ||
             tm.tm_mday < 1 || tm.tm_mday > 31 ||
@@ -39,7 +59,7 @@ namespace json
         {
             throw std::runtime_error("Invalid time " + str);
         }
-
+        //convert to time_t
 #ifdef _MSC_VER
         return _mkgmtime(&tm);
 #else
